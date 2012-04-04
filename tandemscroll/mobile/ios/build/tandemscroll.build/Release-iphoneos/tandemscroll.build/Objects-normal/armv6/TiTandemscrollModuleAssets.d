57ca481df91bd713a5e81abb78fcf8328f947064
John Pataki <john.pataki@yahoo.com>
2012-04-03 23:36:08 -0700

    removing build folder contents from repo branch

--- tandemscroll/mobile/ios/build/tandemscroll.build/Release-iphoneos/tandemscroll.build/Objects-normal/armv6/TiTandemscrollModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/rms/Documents/Logical\ Labs/titanium_open_source_modules/tandemscroll/mobile/ios/Classes/TiTandemscrollModuleAssets.m \
-  /Users/rms/Documents/Logical\ Labs/titanium_open_source_modules/tandemscroll/mobile/ios/Classes/TiTandemscrollModuleAssets.h
