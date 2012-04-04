724a2999b6d6f8e9831bfea208e19fe576827fd5
rmsalter <richard.salter@logicallabs.com>
2012-04-03 17:09:53 -0700

    Changed tandemscroll version to 1.2

---
+++ moddevguide/mobile/ios/build/moddevguide.build/Release-iphoneos/moddevguide.build/Objects-normal/armv6/TiModdevguideModuleAssets.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/rms/Documents/Logical\ Labs/titanium_open_source_modules/moddevguide/mobile/ios/Classes/TiModdevguideModuleAssets.m \
+  /Users/rms/Documents/Logical\ Labs/titanium_open_source_modules/moddevguide/mobile/ios/Classes/TiModdevguideModuleAssets.h
---
+++ paint/mobile/ios/build/paint.build/Release-iphoneos/paint.build/Objects-normal/armv6/TiPaintModuleAssets.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/rms/Documents/Logical\ Labs/titanium_open_source_modules/paint/mobile/ios/Classes/TiPaintModuleAssets.m \
+  /Users/rms/Documents/Logical\ Labs/titanium_open_source_modules/paint/mobile/ios/Classes/TiPaintModuleAssets.h
